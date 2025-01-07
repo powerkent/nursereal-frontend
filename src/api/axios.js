bf3993b929c5082a6efe8cb28cd452c3de5728f6
Quentin Lemoine <quentin.lemoine62580@gmail.com>
2025-01-07 02:31:55 +0100

    feat(14): handle clocking in part 2

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'http://localhost:8080/api',
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/ld+json',
   },
 });
 
